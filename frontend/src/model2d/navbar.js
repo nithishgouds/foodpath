5a12bc2b194b68bae8e37f3de0af445cb36eb55a
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2024-10-08 19:05:52 +0530

    Update navbar.js

--- frontend/src/model2d/navbar.js
+++ frontend/src/model2d/navbar.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Brain from "../guides/brain";
+import Heart from "../guides/heart";
+import Intestine from "../guides/intestine";
+import Liver from "../guides/liver";
+import Lungs from "../guides/lungs";
+import Stomach from "../guides/stomach";
 import './navbarstyle.css';
 
 const Navbar = () => { 
