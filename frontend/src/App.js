ba565e7eb13af444ef377c79bc05566bbc3d799f
anirudhreddyjakka <anirudhreddyjakka001@gmail.com>
2024-10-08 22:56:50 +0530

    dalle

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 
 import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 import HomePage from "./homepage/HomePage";
 function App() {
   return (
+    <Router>
     <div >
       <HomePage/>
+      <Routes>
+          <Route path="/" element={<Home />} />
+          
+          
+        </Routes>
     </div>
+    </Router>
   );
 }
 
--- frontend/src/homepage/HomePage.jsx
+++ frontend/src/homepage/HomePage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Snavbar from "./snavbar.jsx";
 import HeroSection from "./HeroSection.jsx";
 import Footer from "./Footer.jsx";
