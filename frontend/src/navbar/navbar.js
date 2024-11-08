34e31d0004a4a3e8d193d7539688bbec0ebbe0c6
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2024-11-08 22:14:40 +0530

    aa

---
+++ frontend/src/navbar/navbar.js
@@ -0,0 +1,21 @@
+//1C2E3B
+import './navbar.css';
+
+function TextButton(){
+
+}
+
+export default function NNavbar(){
+  return(
+    <>
+    <div class="mnavbar">
+      <div class="navtitle">
+        
+      </div>
+      <div class="navbuttons">
+        
+      </div>
+    </div>
+    </>
+  )
+}
