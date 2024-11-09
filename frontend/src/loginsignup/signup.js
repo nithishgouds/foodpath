a717ec69cc0dbea14ea959093bee9f06a818ee6f
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2024-11-09 21:32:09 +0530

    mma

--- frontend/src/loginsignup/signup.js
+++ frontend/src/loginsignup/signup.js
@@ -1,10 +1,74 @@
+// import './loginsignup.css';
+// import Snavbar from '../homepage/snavbar';
+// import { useState } from 'react';
+
+
+// export default function (){
+
+
+//   return(
+//     <>
+//       <Snavbar/>
+//       <div>
+//       <div class="loginelements">
+//         <div class="emptyspace1">
+//         </div>
+//         <div class="loginpage">
+//           <div class="loginfield">
+//             <div class="loginheadtitle">
+//               Create Account
+//             </div>
+//             <text class="logintitle">
+//               Username
+//             </text>
+//             <input class="logininput"></input>
+//             <text class="logintitle">
+//               Password
+//             </text>
+//             <input class="logininput"></input>
+//             <button class="loginbutton">Sign Up</button>
+//             <t class="logint"  >Go Back to <a class="logina" href="/login">Login</a></t>
+//           </div>
+//         </div>
+//         <div class="emptyspace">
+//         </div>
+//       </div>
+//       </div>
+//     </>
+//   );
+// }
 import './loginsignup.css';
 import Snavbar from '../homepage/snavbar';
 import { useState } from 'react';
+import React from 'react';
+import Axios from 'axios';
 
+export default function Login(){
+  const [username,setUsername]=useState("");
+  const [password,setPassword]=useState("");
+  // const [isSignup, setIsSignup] = useState(false);
 
-export default function (){
+  const toggleMode = () => {
+    setIsSignup(!isSignup);
+  };
 
+  const handleButton = async () => {
+    console.log(username);
+    console.log(password);
+    console.log(isSignup);
+    try{
+    const response = await Axios.post('http://localhost:3001/authRoutes', {
+        // pagetype:isSignup,
+        email:username,
+        password:password
+      });
+      console.log(response.data);
+    }catch(e){
+      console.log(e)
+    }
+
+  }
+  
 
   return(
     <>
@@ -16,18 +80,18 @@ export default function (){
         <div class="loginpage">
           <div class="loginfield">
             <div class="loginheadtitle">
-              Create Account
+              Log In
             </div>
             <text class="logintitle">
-              Username
+              Email
             </text>
-            <input class="logininput"></input>
+            <input class="logininput" onChange={(e)=>{setUsername(e.target.value)}}></input>
             <text class="logintitle">
               Password
             </text>
-            <input class="logininput"></input>
-            <button class="loginbutton">Sign Up</button>
-            <t class="logint"  >Go Back to <a class="logina" href="/login">Login</a></t>
+            <input class="logininput" onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button class="loginbutton" onClick={handleButton}>Login</button>
+            <t class="logint"  >No account?<a class="logina" href="/signup">Sign Up</a></t>
           </div>
         </div>
         <div class="emptyspace">
---
+++ frontend/src/model3dpage.js
@@ -0,0 +1,14 @@
+// import Navbar from "./model2d/navbar";
+import Content from "./model2d/content";
+import React from "react";
+import Snavbar from "./homepage/snavbar";
+
+function Model3dpage(){
+    return(
+        <div>
+            <Snavbar />
+            <Content />
+        </div>
+    );
+}
+export default Model2dpage;
