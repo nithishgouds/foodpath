07b2b81502c71611c5edb6e5c1cb6f97e3ca68b7
Karri Sri Saya Sandeep <srisayasandeepkarri@gmail.com>
2024-11-16 00:55:08 +0530

    changed model UI

--- frontend/src/model3dpage.js
+++ frontend/src/model3dpage.js
@@ -1,16 +1,32 @@
-import Content3d from "./model3d/content3d";
+// import Content3d from "./model3d/content3d";
+// import React from "react";
+// import Header from "./homepagenew/components/Header";
+// import HeaderStationary from "./homepagenew/components/HeaderStationary";
+
+// function Model3dpage(){
+//     return(
+//         <div>
+//             <HeaderStationary />
+//             <div className="3dmodelpage pt-28">
+//             <Content3d />
+//             </div>
+//         </div>
+//     );
+// }
+// export default Model3dpage;
+import Content from "./model3d/content3d";
 import React from "react";
 import Header from "./homepagenew/components/Header";
-import HeaderStationary from "./homepagenew/components/HeaderStationary";
 
-function Model3dpage(){
-    return(
-        <div>
-            <HeaderStationary />
-            <div className="3dmodelpage pt-28">
-            <Content3d />
-            </div>
-        </div>
-    );
+function Model2dpage() {
+  return (
+    <>
+      <div style={{paddingTop:'95px'}}></div>
+      <Header />
+      <div style={{backgroundColor:'#1C2E3B',width:'94%',marginTop:'20px',borderRadius:'20px',padding:'20px',position:'relative',zIndex:'10',diplay:'flex',marginLeft:'53px',marginBottom:'7px'}}>
+      <Content />
+      </div>
+    </>
+  );
 }
-export default Model3dpage;
+export default Model2dpage;
